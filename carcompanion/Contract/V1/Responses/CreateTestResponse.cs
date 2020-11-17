09c8b10ef064915ea5a44aabfcca4f8e61af8579
andre <gombkaa@gmail.com>
2020-11-17 14:18:52 +0100

    Delete test entity

--- carcompanion/Contract/V1/Responses/CreateTestResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace carcompanion.Contract.V1.Responses
-{
-    public class CreateTestResponse
-    {
-        public int Id { get; set; }
-        public string Content { get; set; }
-    }
-}

57b439917e15525ef55ffac424835542c4dc90ca
andre <gombkaa@gmail.com>
2021-01-03 18:51:26 +0100

    Update expense table

--- carcompanion/Data/Migrations/20210103155822_update-expense-table.Designer.cs
+++ carcompanion/Data/Migrations/20210103155822_update-expense-table.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using carcompanion.Data;
 
 namespace carcompanion.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210103155822_update-expense-table")]
+    partial class updateexpensetable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

ca6990f85c7c34209e8b82cce8cf73127abfd379
andre <gombkaa@gmail.com>
2021-01-03 18:51:26 +0100

    Update expense table

---
+++ carcompanion/Data/Migrations/20210103155822_update-expense-table.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace carcompanion.Data.Migrations
+{
+    public partial class updateexpensetable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MileageInterval",
+                table: "Expenses",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndOfDateInterval",
+                table: "Expenses",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Expenses",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Expenses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MileageInterval",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndOfDateInterval",
+                table: "Expenses",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}

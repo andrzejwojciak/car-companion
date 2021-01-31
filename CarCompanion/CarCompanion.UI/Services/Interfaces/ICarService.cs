551033cfac16e1c34b7d86a98203ecef345945ce
andre <gombkaa@gmail.com>
2021-01-31 23:24:32 +0100

    Add create car page and methods

--- CarCompanion/CarCompanion.UI/Services/Interfaces/ICarService.cs
+++ CarCompanion/CarCompanion.UI/Services/Interfaces/ICarService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using CarCompanion.Shared.Contract.V1.Requests.Car;
 using CarCompanion.Shared.Contract.V1.Responses.Car;
 using CarCompanion.Shared.Results;
 
@@ -8,5 +9,6 @@ namespace CarCompanion.UI.Services.Interfaces
     {
         Task<ServiceResult<GetUserCarsResponse>> GetUserCarsAsync();
         Task<ServiceResult<GetCarByIdResponse>> GetCarByIdAsync(string carId);
+        Task<ServiceResult<CreateCarResponse>> CreateCarAsync(CreateCarRequest request);
     }
 }
